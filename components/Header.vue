87b4e95b17e5ab1c0b95855b8b3ab7f442decd61
Kipkoech Kevin <kipkoech.kevin@squaddigital.com>
2024-01-20 03:28:37 +0300

    menu items created and dropdown

--- components/Header.vue
+++ components/Header.vue
@@ -18,10 +18,34 @@
           <Icon name="heroicons:sun" class="w-5 h-5"/>
         </button>
 
-        <button class="flex items-center gap-2">
-          <Icon name="heroicons:user" class="w-5 h-5" />
-          <span class="text-sm font-semibold">John Dough</span>
-        </button>
+        <HMenu as="div" class="relative">
+
+          <HMenuButton class="flex h-9 w-9 shrink-0 items-center overflow-hidden justify-center rounded-full border bg-background">
+              <img src="https://randomuser.me/api/portraits/med/men/34.jpg" class="w-full h-full" alt="Random User Image" />
+          </HMenuButton>
+
+          <HMenuItems class="absolute right-0 z-10 mt-3 w-48 rounded-md border bg-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring">
+            <div class="border-b px-3 py-1.5 text-sm">
+              <p class="font-semibold">Hello John Doe</p>
+              <a href="mailto:john@example.com" class="text-muted-foreground leading-none">
+                 johndoe@example.com
+              </a>
+            </div>
+            <div class="p-1">
+                <template v-for="(profile, i) in profileMenuOptions" :key="i">
+                    <HMenuItem v-if="!profile.divider" v-slot="{ active }">
+                      <NuxtLink :to="profile.url"
+                                :class="[active && 'bg-muted']"
+                                class="inline-flex w-full items-center rounded-md p-2 text-sm font-medium">
+                        {{ profile.title }}
+                      </NuxtLink>
+                    </HMenuItem>
+                  <hr v-if="profile.divider" class="my-1">
+                </template>
+            </div>
+          </HMenuItems>
+
+        </HMenu>
 
       </div>
 
@@ -37,5 +61,17 @@ const mode = useColorMode();
 const toggleMode = () => {
   mode.value = mode.value === 'light' ? 'dark' : 'light';
 }
+
+const profileMenuOptions = [
+  {title: "Profile", url: "/profile"},
+  {title: "Billing", url: "/billing"},
+  {title: "Settings", url: "/settings"},
+  {title: "Team Members", url: "/team-members"},
+  {title: "Sales", url: "/sales"},
+  {title: "Support", url: "/support"},
+  {divider: true},
+  {title: "Sign Out", url: "/sign-out"},
+
+]
 
 </script> 
